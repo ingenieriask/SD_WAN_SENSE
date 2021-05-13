fc578be6c808cfa4a5dca1030f26bf2318bb3f48
ingenieriask <81705337+ingenieriask@users.noreply.github.com>
2021-05-13 17:35:11 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 ###PfSense Script####
 #Requisitos
-mkdir -R /usr/ports/Mk
+mkdir /usr/ports/
+mkdir /usr/ports/Mk
 #Paquetes
+echo FreeBSD: { enabled: yes  }  > /usr/local/etc/pkg/repos/FreeBSD.conf
 cp /usr/local/etc/pkg/repos/pfSense.conf /usr/local/etc/pkg/repos/pfSense.conf.bk
 sed '1d' /usr/local/etc/pkg/repos/pfSense.conf.bk >  /usr/local/etc/pkg/repos/pfSense.conf
 echo "FreeBSD: { enabled: yes }" >> /usr/local/etc/pkg/repos/pfSense.conf
-yes | pkg install git gcc
+yes | pkg install git gcc zerotier
 cat /usr/local/etc/pkg/repos/pfSense.conf.bk > /usr/local/etc/pkg/repos/pfSense.conf
-pkg add http://pkg.freebsd.org/freebsd:12:x86:64/latest/All/zerotier-1.6.5.txz 
 #Directorio
 cd /tmp/
 #Archivos necesarios
@@ -20,6 +21,6 @@ cp -R Mk/* /usr/ports/Mk/
 #Acceso al directorio para copilar el txz
 cd pfSense-pkg-zerotier/
 #Copilar
-make clean ; make package
+make clean ; make package COMPILER_TYPE=gcc
 #Instalar archivo Copilado
 pkg add /tmp/SD_WAN_SENSE/pfSense-pkg-zerotier/work/pkg/pfSense-pkg-zerotier-0.00.1.txz
